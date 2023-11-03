6b223b1430974de0469c78d9c7f6182f67458064
queirozdaniel <danielqueirozak77@gmail.com>
2023-11-02 21:17:33 -0300

    feat: created an endpoint

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    embeddedServer(Netty, port = 4207) {
+        createKtorApplication()
+    }.start(wait = true)
+}
+
+fun Application.createKtorApplication() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello, World!")
+        }
+    }
+
 }
