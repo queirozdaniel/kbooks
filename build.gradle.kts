b8ef41cb7dfae74366bef01efe5fad3ad0fb806e
queirozdaniel <danielqueirozak77@gmail.com>
2023-11-20 22:02:42 -0300

    chore: Adding Gson to work with Json

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:1.4.4")
     implementation("org.slf4j:slf4j-api:2.0.3")
     implementation("com.typesafe:config:1.4.2")
+    implementation("com.google.code.gson:gson:2.10")
 }
 
 tasks.test {
